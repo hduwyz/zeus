d4777e71e7aa1b503c924efc7ce46f145049c082
wangyz <wangyuzhong@cmdi.chinamobile.com>
2020-04-17 11:31:13 +0800

    修改配置文件

--- zeus-demo/zeus-springboot-demo/src/main/java/com/zeus/demo/config/LogConfig.java
+++ zeus-demo/zeus-springboot-demo/src/main/java/com/zeus/demo/config/LogConfig.java
@@ -1,35 +1,36 @@
-//package com.zeus.demo.config;
-//
-//import com.zeus.core.filter.ZeusLogFilter;
-//import com.zeus.core.handler.LogHandler;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class LogConfig {
-//    private Logger logger = LoggerFactory.getLogger(LogConfig.class);
-//
-//    @Bean
-//    public LogHandler logHandler() {
-//        logger.info(">>>>>>>>>>> logInfo config init.");
-//        LogHandler logHandler = new LogHandler();
-//        return logHandler;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean xxlSsoFilterRegistration() {
-//
-//        // xxl-sso, filter init
-//        FilterRegistrationBean registration = new FilterRegistrationBean();
-//
-//        registration.setName("XxlSsoWebFilter");
-//        registration.setOrder(1);
-//        registration.addUrlPatterns("/*");
-//        registration.setFilter(new ZeusLogFilter());
-//
-//        return registration;
-//    }
-//}
+package com.zeus.demo.config;
+
+import com.zeus.core.filter.ZeusLogFilter;
+import com.zeus.core.handler.LogHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SuppressWarnings("unchecked")
+@Configuration
+public class LogConfig {
+    private Logger logger = LoggerFactory.getLogger(LogConfig.class);
+
+    @Bean
+    public LogHandler logHandler() {
+        logger.info(">>>>>>>>>>> logInfo config init.");
+        return new LogHandler();
+    }
+
+    @Bean
+    public FilterRegistrationBean xxlSsoFilterRegistration() {
+
+        // xxl-sso, filter init
+        FilterRegistrationBean registration;
+        registration = new FilterRegistrationBean();
+
+        registration.setName("XxlSsoWebFilter");
+        registration.setOrder(1);
+        registration.addUrlPatterns("/*");
+        registration.setFilter(new ZeusLogFilter());
+
+        return registration;
+    }
+}
