8fec654c06362a9255af92f1ea09d5005acc3134
wangyz <wangyuzhong@cmdi.chinamobile.com>
2020-04-24 11:59:50 +0800

    去除eureka注册中心 改用nacos

--- zeus-uaa/zeus-uaa-shiro/src/main/java/com/zeus/uaa/UaaApplication.java
+++ zeus-uaa/zeus-uaa-shiro/src/main/java/com/zeus/uaa/UaaApplication.java
@@ -4,9 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = "com.zeus.uaa.mapper")
