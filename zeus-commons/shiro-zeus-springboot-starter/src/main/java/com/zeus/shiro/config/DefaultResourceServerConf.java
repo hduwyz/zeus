4ac4fbb93e3d79138178d25b5046582bcbb815dd
wangyz <wangyuzhong@cmdi.chinamobile.com>
2020-07-03 14:59:44 +0800

    删除demo工程

--- zeus-commons/shiro-zeus-springboot-starter/src/main/java/com/zeus/shiro/config/DefaultResourceServerConf.java
+++
@@ -1,7 +0,0 @@
-package com.zeus.shiro.config;
-
-import org.springframework.context.annotation.Import;
-
-@Import({DefaultSecurityHandlerConfig.class, HttpInvokerProxyFactoryBeanConfig.class})
-public class DefaultResourceServerConf {
-}
--- zeus-commons/shiro-zeus-springboot-starter/src/main/java/com/zeus/shiro/config/ShiroPropertiesConfig.java
+++
@@ -1,8 +0,0 @@
-package com.zeus.shiro.config;
-
-import com.zeus.shiro.properties.ShiroProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@EnableConfigurationProperties(ShiroProperties.class)
-public class ShiroPropertiesConfig {
-}
