dccde918003aae6b5ff733350f8f5df26a89ddb3
wangyz <wangyuzhong@cmdi.chinamobile.com>
2020-07-03 14:27:34 +0800

    使用nacos做配置中心

---
+++ zeus-config/src/main/java/com.zeus.config/ConfigApplication.java
@@ -0,0 +1,28 @@
+package com.zeus.config;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@NacosPropertySource(dataId = "springboot2-nacos-config", autoRefreshed = true)
+@SpringBootApplication
+@RestController
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+    @NacosValue(value = "${nacos.test.propertie:123}", autoRefreshed = true)
+    private String testProperties;
+
+    @GetMapping("/test")
+    public String test(){
+        return testProperties;
+    }
+}
